6ef5c085d48c281f3cf6fab8d7ed0cdd5054e87f
Marekp95 <marek-313-95@o2.pl>
2017-12-10 22:42:16 +0100

    small refactor

--- src/main/scala/rest/ProductsController.scala
+++ src/main/scala/rest/ProductsController.scala
@@ -1,10 +1,9 @@
 package rest
 
-import java.util
-
 import org.springframework.beans.factory.annotation.Autowired
-import scala.collection.JavaConverters._
 import org.springframework.web.bind.annotation._
+
+import scala.collection.JavaConverters._
 
 @RestController
 @RequestMapping(Array("products"))
@@ -13,10 +12,8 @@ class ProductsController @Autowired()(productsService: ProductsServiceImpl) {
   @PostMapping(consumes = Array("application/json"))
   @ResponseBody
   def findProducts(@RequestBody parameters: java.util.List[String]): java.util.List[ItemResponse] = {
-    val y = new util.ArrayList[ItemResponse]()
     productsService.getProducts(parameters.asScala.toList)
       .map(x => new ItemResponse(x))
-      .foreach(i => y.add(i))
-    y
+      .asJava
   }
 }
